8e4d3a842aec9ac976375d4bede260b0d852c6af
Bastian Bechtold <basti@bastibe.de>
2014-03-10 15:36:41 +0100

    switched implementation from THREE.js to plain old WebGL
    Much more code, but at least I now know what's going on.

---
+++ communication.js
@@ -0,0 +1,57 @@
+var ws = new WebSocket("ws://localhost:8888/websocket");
+ws.binaryType = 'arraybuffer';
+
+function send_msg(type, content) {
+    ws.send(JSON.stringify({
+        "type": type,
+        "content": content
+    }));
+}
+
+function request_spectrogram(filename, nfft, overlap) {
+    send_msg("request_spectrogram", {
+        "filename": filename,
+        "nfft": nfft,
+        "overlap": overlap
+    });
+}
+
+function send_status(status) {
+    send_msg("status", status);
+}
+
+ws.onopen = function() {
+    send_status("Hello, World!");
+};
+
+ws.onmessage = function(event) {
+    if (event.data.constructor.name === "ArrayBuffer") {
+        var header_len = new Int32Array(event.data, 0, 1)[0];
+        var header = String.fromCharCode.apply(null, new Uint8Array(event.data, 4, header_len));
+        try {
+            header = JSON.parse(header)
+        } catch(e) {
+            console.error("Could not parse header of binary message:", e.message)
+            return
+        }
+        var data = new Float32Array(event.data, header_len+4);
+        if (header.type === 'spectrogram') {
+            var dummyTexture = new Float32Array(256);
+            for (var i in data) { dummyTexture[i] = i/dummyTexture.byteLength*4; }
+        }
+    } else {
+        try {
+            msg = JSON.parse(event.data)
+        } catch(e) {
+            console.error("Could not parse message:", e.message)
+            return
+        }
+        console.log(msg.type, msg.content);
+    }
+};
+
+var container = document.getElementById('spectrogram');
+
+spectrogram.onclick = function(event) {
+    request_spectrogram('thistle.wav')
+};
--- spectrogram.js
+++ spectrogram.js
@@ -1,69 +1,189 @@
-var ws = new WebSocket("ws://localhost:8888/websocket");
-ws.binaryType = 'arraybuffer';
+var canvas;
+var gl;
+var squareVerticesBuffer;
+var squareVerticesTextureCoordBuffer;
+var mvMatrix;
+var shaderProgram;
+var vertexPositionAttribute;
+var textureCoordAttribute;
+var perspectiveMatrix;
 
-function send_msg(type, content) {
-    ws.send(JSON.stringify({
-        "type": type,
-        "content": content
-    }));
-}
+var specImage;
+var specTexture;
 
-function request_spectrogram(filename, nfft, overlap) {
-    send_msg("request_spectrogram", {
-        "filename": filename,
-        "nfft": nfft,
-        "overlap": overlap
-    });
-}
+function start() {
+    canvas = document.getElementById('spectrogram');
+    gl = initWebGL(canvas);
 
-function send_status(status) {
-    send_msg("status", status);
-}
-
-ws.onopen = function() {
-    send_status("Hello, World!");
-};
-
-ws.onmessage = function(event) {
-    if (event.data.constructor.name === "ArrayBuffer") {
-        var header_len = new Int32Array(event.data, 0, 1)[0];
-        var header = String.fromCharCode.apply(null, new Uint8Array(event.data, 4, header_len));
-        try {
-            header = JSON.parse(header)
-        } catch(e) {
-            console.error("Could not parse header of binary message:", e.message)
-            return
-        }
-        var data = new Float32Array(event.data, header_len+4);
-    } else {
-        try {
-            msg = JSON.parse(event.data)
-        } catch(e) {
-            console.error("Could not parse message:", e.message)
-            return
-        }
-        console.log(msg.type, msg.content);
+    if (gl) {
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthFunc(gl.LEQUAL);
+        gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+        gl.viewport(0, 0, canvas.width, canvas.height); // set resolution
+        console.log('starting');
+        initShaders();
+        initBuffers();
+        initTextures();
+        setInterval(drawScene, 15);
     }
-};
+}
 
-document.onclick = function(event) {
-    request_spectrogram('thistle.wav')
-};
+function initWebGL(canvas) {
+    gl = null;
+    try {
+        gl = canvas.getContext('webgl');
+    } catch (e) {
+        alert('Could not initialize WebGL');
+        gl = null;
+    }
+    return gl;
+}
 
-scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-var geometry = new THREE.CubeGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-camera.position.z = 5;
-var render = function () {
-    requestAnimationFrame(render);
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
-    renderer.render(scene, camera);
-};
-render();
+function initShaders() {
+    var fragmentShader = getShader(gl, 'fragmentShader');
+    var vertexShader = getShader(gl, 'vertexShader');
+
+    shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert("unable to initialize shader program.");
+    }
+
+    gl.useProgram(shaderProgram);
+
+
+    vertexPositionAttribute = gl.getAttribLocation(shaderProgram, 'aVertexPosition');
+    gl.enableVertexAttribArray(vertexPositionAttribute);
+    textureCoordAttribute = gl.getAttribLocation(shaderProgram, 'aTextureCoord');
+    gl.enableVertexAttribArray(textureCoordAttribute);
+}
+
+function getShader(gl, id) {
+    var shaderScript, theSource, currentChild, shader;
+
+    shaderScript = document.getElementById(id);
+
+    if (!shaderScript) {
+        return null;
+    }
+
+    theSource = "";
+    currentChild = shaderScript.firstChild;
+
+    while (currentChild) {
+        if (currentChild.nodeType == currentChild.TEXT_NODE) {
+            theSource += currentChild.textContent;
+        }
+        currentChild = currentChild.nextSibling;
+    }
+
+    if (shaderScript.type == "x-shader/x-fragment") {
+        shader = gl.createShader(gl.FRAGMENT_SHADER);
+    } else if  (shaderScript.type == "x-shader/x-vertex") {
+        shader = gl.createShader(gl.VERTEX_SHADER);
+    } else {
+        return null;
+    }
+
+    gl.shaderSource(shader, theSource);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.log(shaderScript.type, theSource);
+        alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));
+        return null;
+    }
+
+    return shader;
+}
+
+var horizAspect = 580.0/640.0;
+
+function initBuffers() {
+    squareVerticesBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
+
+    var vertices = [
+         1.0,  1.0,  0.0,
+        -1.0,  1.0,  0.0,
+         1.0, -1.0,  0.0,
+        -1.0, -1.0,  0.0
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+    squareVerticesTextureCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesTextureCoordBuffer);
+
+    var textureCoordinates = [
+        1.0, 1.0,
+        0.0, 1.0,
+        1.0, 0.0,
+        0.0, 0.0
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
+}
+
+function initTextures() {
+    specTexture = gl.createTexture();
+    specImage = new Image();
+    specImage.onload = function() { handleTextureLoaded(specImage, specTexture); }
+    specImage.src = "crate.jpg";
+}
+
+function handleTextureLoaded(image, texture) {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+function drawScene() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    perspectiveMatrix = makePerspective(45, 640.0/480.0, 0.1, 100.0);
+
+    loadIdentity();
+    mvTranslate([-0.0, 0.0, -6.0]);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, specTexture);
+    gl.uniform1i(gl.getUniformLocation(shaderProgram, 'uSampler'), 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
+    gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesTextureCoordBuffer);
+    gl.vertexAttribPointer(textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
+
+    setMatrixUniforms();
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
+
+function loadIdentity() {
+    mvMatrix = Matrix.I(4);
+}
+
+function multMatrix(m) {
+    mvMatrix = mvMatrix.x(m);
+}
+
+function mvTranslate(v) {
+    multMatrix(Matrix.Translation($V([v[0], v[1], v[2]])).ensure4x4());
+}
+
+function setMatrixUniforms() {
+    var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+    gl.uniformMatrix4fv(pUniform, false, new Float32Array(perspectiveMatrix.flatten()));
+
+    var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+    gl.uniformMatrix4fv(mvUniform, false, new Float32Array(mvMatrix.flatten()));
+}
