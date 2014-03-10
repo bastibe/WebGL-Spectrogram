0af703396d2d16b6a1c0abc5413baf313ba9eded
Bastian Bechtold <basti@bastibe.de>
2014-03-10 16:57:32 +0100

    disabled drawing when window is not showing.

--- spectrogram.js
+++ spectrogram.js
@@ -174,6 +174,10 @@ function handleSpectrogramLoaded(spectrogram, nfreqs, nblocks) {
 }
 
 function drawScene() {
+    if (document.hidden) {
+        return
+    }
+
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     perspectiveMatrix = makePerspective(45, 640.0/480.0, 0.1, 100.0);
