96f245b4eed38fa09eba6735e29c447b7e1391ff
Bastian Bechtold <basti@bastibe.de>
2014-03-11 15:35:47 +0100

    implemented drawing of arbitrarily long audio files.
    Spectrograms are now cut up into chunks of MAX_TEXTURE_SIZE and drawn next to one another.

--- spectrogram.js
+++ spectrogram.js
@@ -6,10 +6,12 @@ var vertexPositionAttribute;
 var textureCoordAttribute;
 var samplerUniform;
 
-var vertexPositionBuffer;
-var textureCoordBuffer;
+var vertexPositionBuffers;
+var textureCoordBuffers;
 
-var specTexture;
+var spectrogramTextures;
+
+var dirty = false;
 
 function start() {
     canvas = document.getElementById('spectrogram');
@@ -22,8 +24,7 @@ function start() {
     gl.clear(gl.COLOR_BUFFER_BIT);
     logInfo();
     initShaders();
-    initBuffers();
-    initTextures();
+    loadSpectrogram(new Float32Array(256), 16, 16);
     setInterval(drawScene, 15);
 }
 
@@ -31,6 +32,7 @@ function resizeCanvas() {
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
     gl.viewport(0, 0, canvas.width, canvas.height);
+    dirty = true;
 }
 
 function logInfo() {
@@ -123,65 +125,79 @@ function getShader(gl, id) {
     return shader;
 }
 
-var horizAspect = 580.0/640.0;
-
-function initBuffers() {
-    vertexPositionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
-
-    var vertices = [
-         1.0,  1.0,  0.0,
-         1.0, -1.0,  0.0,
-        -1.0,  1.0,  0.0,
-        -1.0, -1.0,  0.0
-    ];
-
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
-    textureCoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
-
-    var textureCoordinates = [
-        1.0, 1.0,
-        0.0, 1.0,
-        1.0, 0.0,
-        0.0, 0.0
-    ];
-
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
-}
-
-function initTextures() {
-    specTexture = gl.createTexture();
-    loadSpectrogram(new Float32Array(256), 16, 16);
-}
-
 function loadSpectrogram(spectrogram, nblocks, nfreqs) {
-    gl.bindTexture(gl.TEXTURE_2D, specTexture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.LUMINANCE, nfreqs, nblocks, 0, gl.LUMINANCE, gl.FLOAT, spectrogram);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.bindTexture(gl.TEXTURE_2D, null);
+    for (var i in spectrogramTextures) {
+        gl.deleteBuffer(vertexPositionBuffers[i]);
+        gl.deleteBuffer(textureCoordBuffers[i]);
+        gl.deleteTexture(spectrogramTextures[i]);
+    }
+
+    var max_width = gl.getParameter(gl.MAX_TEXTURE_SIZE);
+    var num_textures = Math.ceil(nblocks / max_width);
+    var max_x_coord = nblocks / max_width;
+    if (max_x_coord < 1) { max_x_coord = 1; }
+    vertexPositionBuffers = new Array(num_textures);
+    textureCoordBuffers = new Array(num_textures);
+    spectrogramTextures = new Array(num_textures);
+
+    console.log("Cutting up spectrogram in "+num_textures+" textures.");
+
+    for (var i=0; i<num_textures; i++) {
+        var min_x = i/max_x_coord;
+        var max_x = (i < num_textures-1) ? (i+1)/max_x_coord : 1;
+
+        vertexPositionBuffers[i] = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffers[i]);
+        var vertices = [
+            max_x * 2 - 1,  1.0,
+            max_x * 2 - 1, -1.0,
+            min_x * 2 - 1,  1.0,
+            min_x * 2 - 1, -1.0
+        ];
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+        textureCoordBuffers[i] = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffers[i]);
+        var textureCoordinates = [
+            1.0, 1.0,
+            0.0, 1.0,
+            1.0, 0.0,
+            0.0, 0.0
+        ];
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
+
+        var blocks = (i < num_textures-1) ? max_width : nblocks-(i*max_width);
+        var data = spectrogram.subarray(i*max_width*nfreqs);
+        spectrogramTextures[i] = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, spectrogramTextures[i]);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.LUMINANCE, nfreqs, blocks, 0, gl.LUMINANCE, gl.FLOAT, data);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    }
+    dirty = true;
 }
 
 function drawScene() {
-    if (document.hidden) {
+    if (document.hidden || !dirty) {
         return
     }
 
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.activeTexture(gl.TEXTURE0); // add integers for other samplers
-    gl.bindTexture(gl.TEXTURE_2D, specTexture);
-    gl.uniform1i(samplerUniform, 0);
+    for (var i=0; i<spectrogramTextures.length; i++) {
+        gl.activeTexture(gl.TEXTURE0+i);
+        gl.bindTexture(gl.TEXTURE_2D, spectrogramTextures[i]);
+        gl.uniform1i(samplerUniform, i);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
-    gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffers[i]);
+        gl.vertexAttribPointer(vertexPositionAttribute, 2, gl.FLOAT, false, 0, 0);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
-    gl.vertexAttribPointer(textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
+        gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffers[i]);
+        gl.vertexAttribPointer(textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
 
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    }
+    dirty = false;
 }
