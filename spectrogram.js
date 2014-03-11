f68bafc9c08ca1f06bd5acf89517e5e412706fd4
Bastian Bechtold <basti@bastibe.de>
2014-03-11 16:34:11 +0100

    implemented zooming and panning.
    Also moved some drawing code out of the hot loop.
    Zooming still zooms into the center of the spectrogram and panning still allows to pan out of the spectrogram. But it works.

--- spectrogram.js
+++ spectrogram.js
@@ -1,20 +1,25 @@
-var canvas;
+var canvas = document.getElementById('spectrogram');
+
 var gl;
 var shaderProgram;
 
 var vertexPositionAttribute;
 var textureCoordAttribute;
+
 var samplerUniform;
+var zoomUniform;
 
 var vertexPositionBuffers;
-var textureCoordBuffers;
+var textureCoordBuffer;
 
 var spectrogramTextures;
 
 var dirty = false;
+var zoom = 1.0;
+var pan_x = 0.0;
+var pan_y = 0.0;
 
 function start() {
-    canvas = document.getElementById('spectrogram');
     gl = initWebGL(canvas);
 
     window.addEventListener("resize", resizeCanvas, false);
@@ -85,6 +90,7 @@ function initShaders() {
     gl.enableVertexAttribArray(textureCoordAttribute);
 
     samplerUniform = gl.getUniformLocation(shaderProgram, 'uSampler');
+    zoomUniform = gl.getUniformLocation(shaderProgram, 'mZoom');
 }
 
 function getShader(gl, id) {
@@ -128,19 +134,28 @@ function getShader(gl, id) {
 function loadSpectrogram(spectrogram, nblocks, nfreqs) {
     for (var i in spectrogramTextures) {
         gl.deleteBuffer(vertexPositionBuffers[i]);
-        gl.deleteBuffer(textureCoordBuffers[i]);
         gl.deleteTexture(spectrogramTextures[i]);
     }
+    gl.deleteBuffer(textureCoordBuffer);
 
     var max_width = gl.getParameter(gl.MAX_TEXTURE_SIZE);
     var num_textures = Math.ceil(nblocks / max_width);
     var max_x_coord = nblocks / max_width;
     if (max_x_coord < 1) { max_x_coord = 1; }
     vertexPositionBuffers = new Array(num_textures);
-    textureCoordBuffers = new Array(num_textures);
     spectrogramTextures = new Array(num_textures);
 
     console.log("Cutting up spectrogram in "+num_textures+" textures.");
+
+    textureCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+    var textureCoordinates = [
+        1.0, 1.0,
+        0.0, 1.0,
+        1.0, 0.0,
+        0.0, 0.0
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
 
     for (var i=0; i<num_textures; i++) {
         var min_x = i/max_x_coord;
@@ -155,16 +170,6 @@ function loadSpectrogram(spectrogram, nblocks, nfreqs) {
             min_x * 2 - 1, -1.0
         ];
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
-        textureCoordBuffers[i] = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffers[i]);
-        var textureCoordinates = [
-            1.0, 1.0,
-            0.0, 1.0,
-            1.0, 0.0,
-            0.0, 0.0
-        ];
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
 
         var blocks = (i < num_textures-1) ? max_width : nblocks-(i*max_width);
         var data = spectrogram.subarray(i*max_width*nfreqs);
@@ -186,6 +191,16 @@ function drawScene() {
 
     gl.clear(gl.COLOR_BUFFER_BIT);
 
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+    gl.vertexAttribPointer(textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
+
+    var zoomMatrix = [
+        zoom, 0.0,  pan_x,
+        0.0,  zoom, -pan_y,
+        0.0,  0.0,  1.0
+    ];
+    gl.uniformMatrix3fv(zoomUniform, gl.FALSE, zoomMatrix);
+
     for (var i=0; i<spectrogramTextures.length; i++) {
         gl.activeTexture(gl.TEXTURE0+i);
         gl.bindTexture(gl.TEXTURE_2D, spectrogramTextures[i]);
@@ -194,10 +209,25 @@ function drawScene() {
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffers[i]);
         gl.vertexAttribPointer(vertexPositionAttribute, 2, gl.FLOAT, false, 0, 0);
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffers[i]);
-        gl.vertexAttribPointer(textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
-
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
     }
     dirty = false;
+}
+
+var zoom = 1.0;
+
+canvas.onwheel = function(wheel) {
+    if (wheel.shiftKey) {
+        zoom *= Math.pow(0.99, wheel.deltaY);
+        zoom = Math.max(1.0, zoom);
+        dirty = true;
+    } else {
+        pan_y += 0.001 * wheel.deltaY;
+        pan_x += 0.001 * wheel.deltaX;
+        dirty = true;
+    }
+}
+
+canvas.onclick = function() {
+    console.log("click")
 }
